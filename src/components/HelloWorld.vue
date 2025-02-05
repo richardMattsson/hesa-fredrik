14011de22a4fbf7563f2805be13aa8c0ff1bd0ac
richardMattsson <mattsson.richard@gmail.com>
2025-02-05 09:34:04 +0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    props: {
+      msg: String
+    }
+  };
+</script>
+
+<template>
+  <img alt="" src="/assets/fox.jpeg" />
+  <p>{{ msg }}</p>
+</template>
+
+<style>
+  p {
+    font-weight: bold;
+  }
+</style>
