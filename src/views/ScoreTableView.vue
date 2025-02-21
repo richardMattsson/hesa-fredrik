35207a40ab23f4f2130076d1a352e3fc55163a30
Louise <annica91louise@gmail.com>
2025-02-21 13:34:45 +0100

    Add NavigationComp and ScoreComp to get a seperated navigationmenu and scorelist

--- src/views/ScoreTableView.vue
+++ src/views/ScoreTableView.vue
@@ -1,4 +1,43 @@
 
 <template>
-<h1>vue </h1>
+<section class="section-resulttable">
+      <table>
+        <thead>
+          <tr>
+            <th>Spelare</th>
+            <th>Resultat</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="results in resultData">
+            <td>{{ results.player }}</td>
+            <td>{{ results.result }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <section class="section-restart">
+      <input type="button" @click="restartQuiz" value="Starta om quiz" />
+    </section>
 </template>
+<script>
+export default{
+    data(){
+        return{
+            resultData: JSON.parse(localStorage.getItem('savedResult'))
+        }
+    },
+
+    methods: {
+        restartQuiz() {
+      this.$router.push('/question');
+    },
+    }
+}
+
+</script>
+<style>
+.section-resulttable {
+    background-color: aqua;
+}
+</style>
