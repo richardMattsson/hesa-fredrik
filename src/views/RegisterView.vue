bccdad0efc65eeece32b10e44e5b541ee4647fc7
Louise <annica91louise@gmail.com>
2025-03-05 16:53:39 +0100

    Restored code before last merged/delete changes

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,46 +1,49 @@
 <script>
-import { useAuthStore } from '../stores/useAuthStore'
-  
-export default {
+  import { useQuizStore } from '../stores/quizStore';
+
+  export default {
     data() {
       return {
-        authStore: useAuthStore(),
         points: JSON.parse(localStorage.getItem('points')),
-        numberOfQuestions: JSON.parse(
-          localStorage.getItem('numberOfQuestions')
-        ),
+        numberOfQuestions: JSON.parse(localStorage.getItem('numberOfQuestions')),
+
         newPlayer: { player: '', result: '' },
-        playerName:  '',
+        playerName: '',
         resultData: JSON.parse(localStorage.getItem('savedResult')) || [],
+        quizStore: useQuizStore()
       };
     },
+
     methods: {
       onSave() {
-        if (this.currentUser){
-          this.newPlayer.player = this.currentUser.username
-        }
-        else{
-          this.newPlayer.player = this.playerName;
-        }
+        this.newPlayer.player = this.playerName;
         this.newPlayer.result = this.points;
-        this.resultData.push(this.newPlayer);
-        this.resultData.sort((a, b) => b.result - a.result);
-        this.newPlayer = null;
-        localStorage.setItem('savedResult', JSON.stringify(this.resultData));
-        console.log('Ny spelare: ', this.resultData);
-        this.$router.push('/scoretable');
-      },
 
+        // hämtar från local
+        let existingResults = JSON.parse(localStorage.getItem('savedResult')) || [];
+
+        // lägger till score från pinia
+
+        existingResults.push({player: this.playerName, result: this.quizStore.score});
+
+        existingResults.sort((a, b) => b.result - a.result);
+
+        // Sparar tillbaks till localstorage
+
+        localStorage.setItem('savedResult', JSON.stringify(existingResults));
+
+        console.log('Updated saved results:', existingResults);
+
+        this.$router.push('/scoretable');
+
+      },
       restartQuiz() {
+        this.quizStore.resetQuiz();
         this.$router.push('/question');
       }
     },
-    computed: {
-      currentUser(){
-      return this.authStore.currentUser
-    }
-    }
   };
+
 </script>
 
 <template>
@@ -50,12 +53,7 @@ export default {
         <p id="quiz-result">{{ quizStore.score }} rätta svar av 7</p>
         <label id="container-input-name" for="">
           Vill du spara ditt resultat?
-          <input v-if="!currentUser"
-            id="input-name"
-            v-model="playerName"
-            type="text"
-            placeholder="Namn"
-          />
+          <input  id="input-name" v-model="playerName" type="text" placeholder="Namn" />
         </label>
 
         <section id="section-save-button">
@@ -64,81 +62,80 @@ export default {
       </form>
     </section>
     <section class="section-restart">
-      <input
-        class="restart-button"
-        type="button"
-        @click="restartQuiz"
-        value="Starta om quiz"
-      />
+      <input class="restart-button" type="button" @click="restartQuiz" value="Starta om quiz" />
     </section>
   </article>
 </template>
+
 <style scoped>
-  .container-result {
-    background-color: #ffda00;
-    display: grid;
-    gap: 5px;
-    padding: 5px;
-    grid: auto auto / auto auto auto;
-    height: 100vh;
-  }
+.container-result {
+  background-color: #ffda00;
+  display: grid;
+  gap: 5px;
+  padding: 5px;
+  grid: auto auto / auto auto auto;
+  height: 100vh;
+}
 
-  .section-form {
-    grid-area: 1 / 1 / span 1 / span 3;
-  }
+.section-form {
+  grid-area: 1 / 1 / span 1 / span 3;
+}
 
-  #container-form {
-    display: grid;
-    grid: auto auto / auto auto;
-    /* border: 3px solid rgb(18, 179, 48); */
-  }
+#container-form {
+  display: grid;
+  grid: auto auto / auto auto;
+  /* border: 3px solid rgb(18, 179, 48); */
+}
 
-  #quiz-result {
-    /* border: 3px solid rgb(206, 65, 157); */
-    grid-area: 1 / 1 / span 1 / span 2;
-  }
+#quiz-result {
+  /* border: 3px solid rgb(206, 65, 157); */
+  grid-area: 1 / 1 / span 1 / span 2;
+}
 
-  #container-input-name {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    justify-content: space-evenly;
-    /* border: 3px solid rgb(36, 23, 172); */
-    grid-area: 2 / 1 / span 1 / span 1;
-  }
+#container-input-name {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-evenly;
+  /* border: 3px solid rgb(36, 23, 172); */
+  grid-area: 2 / 1 / span 1 / span 1;
+}
 
-  #input-name {
-    margin-top: 5px;
-    width: 250px;
-    height: 80%;
-  }
+#input-name {
+  margin-top: 5px;
+  width: 250px;
+  height: 80%;
+}
 
-  #section-save-button {
-    /* border: 3px solid rgb(179, 17, 17); */
-    grid-area: 2 / 2 / span 1 / span 1;
-    /* width: 100px;
+#section-save-button {
+  /* border: 3px solid rgb(179, 17, 17); */
+  grid-area: 2 / 2 / span 1 / span 1;
+  /* width: 100px;
   height: 50px; */
-    display: flex;
-    justify-content: center;
-    align-items: end;
-    padding-left: 10px;
-  }
-  #save-button {
-    width: 80px;
-    height: 40px;
-    border-radius: 4px;
-  }
-  .section-link {
-    grid-area: 2 / 1 / span 1 / span 1;
-  }
-  .section-restart {
-    display: flex;
-    justify-content: center;
-  }
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  padding-left: 10px;
+}
 
-  .restart-button {
-    width: 100px;
-    height: 40px;
-    border-radius: 4px;
-  }
+#save-button {
+  width: 80px;
+  height: 40px;
+  border-radius: 4px;
+}
+
+.section-link {
+  grid-area: 2 / 1 / span 1 / span 1;
+}
+
+.section-restart {
+  display: flex;
+  justify-content: center;
+}
+
+.restart-button {
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+}
 </style>
