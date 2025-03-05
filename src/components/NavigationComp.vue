a3771532e5d5521864a44c7d9a55f2795f5852c3
Louise <annica91louise@gmail.com>
2025-03-05 15:05:03 +0100

    Edit styling on nav-menu to fit in desktop- and mobileview, Edit style on HomeView, LoginView and CreateUserView

--- src/components/NavigationComp.vue
+++ src/components/NavigationComp.vue
@@ -1,100 +1,119 @@
 <script>
+import { RouterLink } from 'vue-router';
 import { useAuthStore } from '../stores/useAuthStore';
 
-export default{
-  data(){
+export default {
+  data() {
     return {
       authStore: useAuthStore()
     }
   },
   computed: {
-    currentUser(){
+    currentUser() {
       return this.authStore.currentUser
     }
   }
 }
 </script>
 <template>
-  <header id="container-header">
-    <nav id="container-nav">
-      <ul id="container-ul">
-        <li>
-          <a v-if="currentUser" class="container-links" href="#" @click.prevent="authStore.logout()">Logga ut</a>
-          <a v-else class="container-links" href="#/login" >Logga in</a></li>
-        <li class="container-links" id="nav-home">
-          <RouterLink to="/">Hem</RouterLink>
-        </li>
-        <li class="container-links" id="nav-information">
-          <RouterLink to="/information">Information</RouterLink>
-        </li>
-        <li class="container-links" id="nav-leaderboard">
-          <RouterLink to="/scoretable">Ledartavla</RouterLink>
-        </li>
-        <li class="container-links" id="nav-tutorial">
-          <RouterLink to="/tutorial/introduktion">Övningar</RouterLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <nav id="container-nav">
+    <ul id="container-ul">
+      <li class="container-links" id="nav-home">
+        <RouterLink to="/">Hem</RouterLink>
+      </li>
+      <li class="container-links" id="nav-information">
+        <RouterLink to="/information">Information</RouterLink>
+      </li>
+      <li class="container-links" id="nav-leaderboard">
+        <RouterLink to="/scoretable">Ledartavla</RouterLink>
+      </li>
+      <li class="container-links" id="nav-tutorial">
+        <RouterLink to="/tutorial/introduktion">Övningar</RouterLink>
+      </li>
+      <li>
+        <a v-if="currentUser" class="container-links" href="#" @click.prevent="authStore.logout()">Logga ut</a>
+        <a v-else class="container-links" href="#/login">Logga in</a>
+      </li>
+    </ul>
+  </nav>
+
+  <!--
   <footer id="container-footer">
-    <!--<p class="info-text">Quiz om förberedelser inför en eventuell kris.</p>
+   <p class="info-text">Quiz om förberedelser inför en eventuell kris.</p>
     <p class="info-text">
       Av: Richard Mattsson, Kåre Lindqvist, Louise Nensén Lagnefors
-    </p>-->
-    <p v-if="currentUser" >Inloggad som: {{currentUser.username}}</p>
-    <p v-else >Du är inte inloggad</p>
-  </footer>
+    </p>
+  </footer> 
+  -->
+
 </template>
 <style scoped>
-  #container-header {
-    background-color: #333;
-    color: whitesmoke;
-  }
-  #container-nav {
-    /* background-color: pink; */
-    display: flex;
-    flex-direction: row;
-    /* flex-grow: 1; */
-  }
-  #container-ul {
-    width: 100%;
-    margin: 0;
-    padding: 0;
-    display: flex;
-    align-content: center;
-    justify-content: space-around;
-    list-style-type: none;
-  }
-  .container-links {
-    height: 2rem;
-    display: flex;
-    /* justify-content: center; */
-    padding: 0.2rem;
-    align-items: center;
-  }
-  .container-links:hover {
-    cursor: pointer;
-    background-color: #242222;
-  }
-  #container-footer {
-    background-color: #333;
-    color: whitesmoke;
-    height: 2rem;
-    padding: 0.2rem;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    margin-top: auto; /* Pushes footer to the bottom */
-  }
-  .info-text {
-    margin: 0;
-  }
+#container-nav {
+  background-color: #333;
+  color: whitesmoke;
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem 0;
+}
 
-  @media screen and (max-width: 428px) {
-    #container-footer {
-      display: none;
-    }
+#container-ul {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  align-content: center;
+  justify-content: space-around;
+  list-style-type: none;
+}
+
+.container-links {
+  display: flex;
+  align-items: center;
+  height: 1.5rem;
+  padding: 0.3rem 1rem;
+  border-radius: 6px;
+}
+
+.container-links:hover {
+  cursor: pointer;
+  background-color: #444;
+}
+
+/*
+  #container-footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 1.5rem;
+    padding: 0.5rem;
+    background-color: #333;
+    color: whitesmoke;
   }
+*/
+
+.info-text {
+  margin: 0;
+}
+
+
+@media screen and (max-width: 428px) {
+
+  /*
+   container-footer {
+   display: none;
+   }
+ */
+ #container-ul {
+  font-size: small;
+ }
+
+ .container-links {
+  height: 1rem;
+  padding: 0.3rem 0.5rem;
+  border-radius: 6px;
+}
+
+}
 </style>
--- src/views/CreateUserView.vue
+++ src/views/CreateUserView.vue
@@ -26,8 +26,8 @@ export default {
                     this.password = ''
                     this.confirmPassword = ''
                 }
-            }  
-            catch(error){
+            }
+            catch (error) {
                 console.error("Registreringsfel: ", error)
                 this.message = "Ett oväntat fel uppstod vid registrering"
             }
@@ -37,30 +37,74 @@ export default {
 </script>
 <template>
     <article id="article-create-account">
-        <h1>Registrera Konto</h1>
         <form id="form-create-account" @submit.prevent="register">
-            <div>
-                <label for="username">Användarnamn</label>
+            <h1>Registrera Konto</h1>
+            <div class="div-login">
                 <input id="username" v-model="username" placeholder="Användarnamn" required>
             </div>
-            <div>
-                <label for="password">Lösenord</label>
+            <div class="div-login">
                 <input id="password" type="password" v-model="password" placeholder="Lösenord" required>
             </div>
-            <div>
-                <label for="confirm-password">Bekräfta Lösenord</label>
+            <div class="div-login">
                 <input id="confirm-password" type="password" v-model="confirmPassword" placeholder="Bekräfta Lösenord"
                     required>
             </div>
-            <button type="submit">Registrera</button>
+            <button id="register-button" type="submit">Registrera</button>
             <p v-if="message">{{ message }}</p>
         </form>
-        <p> Har du redan ett konto ? <router-link to="/login">Logga in här</router-link></p>
+        <hr>
+        <router-link to="/login"><button id="login-nav-btn">Logga in här</button></router-link>
     </article>
 </template>
+
 <style scoped>
+#article-create-account {
+    margin: auto;
+    background-color: white;
+    padding: 2rem 2rem;
+    border-radius: 8px;
+    box-shadow: 0 5px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+
 h1 {
-    font-size: 2rem;
-    margin: 1rem 0;
+    margin-top: auto;
+    margin-bottom: 1rem;
+}
+
+.div-login {
+    margin-bottom: 1rem;
+}
+
+button,
+input {
+    box-sizing: border-box;
+    height: 2rem;
+    width: 100%;
+    border-radius: 4px;
+}
+
+input {
+    border: 1px solid black;
+    background-color: white;
+    padding-left: 10px;
+}
+
+button {
+    border-style: none;
+    color: whitesmoke;
+    font-weight: 750;
+    font-size: small;
+}
+
+#register-button {
+    background-color: #FF8000;
+}
+
+#login-nav-btn {
+    background-color: #32CD32;
+}
+
+hr {
+    margin: 20px 0;
 }
 </style>
